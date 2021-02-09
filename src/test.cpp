db8458dc1b36e207967a22683c579ffd39690817
ngoduyanh <ngoduyanh.chip@gmail.com>
2021-02-09 21:52:08 +0700

    Added error handling in test

--- src/test.cpp
+++ src/test.cpp
@@ -4,25 +4,29 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    if(argc == 0)   printf("Please insert your script file in the command line args you mf");
+    if(argc == 1)   printf("Please insert your script file in the command line args you mf");
 
-    std::ifstream input(argv[0]);
-    auto result = tcr::parse([&]() -> std::optional<std::string> {
-        std::string line;
-        if(std::getline(input, line))   return line;    else return {};
-    });
+    std::ifstream input(argv[1]);
+    try {
+        auto result = tcr::parse([&]() -> std::optional<std::string> {
+            std::string line;
+            if(std::getline(input, line))   return line;    else return {};
+        });
 
-    for(auto msg : result.messages) {
-        std::cout << msg.author.name << " (r=" << msg.author.nameColor[0] << ", g=" << msg.author.nameColor[1] << ", b=" << msg.author.nameColor[2] << ", a=" << msg.author.nameColor[3] << ", icons=";
-        
-        if(msg.author.icons.empty()) {
-            std::cout << "empty";
-        } else {
-            std::cout << "[" << msg.author.icons[0];
-            for(size_t i = 1; i < msg.author.icons.size(); i++)    std::cout << ", " << msg.author.icons[i];
-            std::cout << "]";
+        for(const auto& msg : result.messages) {
+            std::cout << msg.author.name << " (r=" << msg.author.nameColor[0] << ", g=" << msg.author.nameColor[1] << ", b=" << msg.author.nameColor[2] << ", a=" << msg.author.nameColor[3] << ", icons=";
+            
+            if(msg.author.icons.empty()) {
+                std::cout << "empty";
+            } else {
+                std::cout << "[" << msg.author.icons[0];
+                for(size_t i = 1; i < msg.author.icons.size(); i++)    std::cout << ", " << msg.author.icons[i];
+                std::cout << "]";
+            }
+
+            std::cout << ") " << msg.content << std::endl;
         }
-
-        std::cout << ") " << msg.content << std::endl;
+    } catch (std::runtime_error& err) {
+        printf("An error occured: %s\n", err.what());
     }
 }
